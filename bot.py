df1e14db5161028722af91b5bf677ea3910f7326
crush-05 <crushalonex5@gmail.com>
2025-03-30 13:52:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ def home():
     return "Bot is running"
 
 def run_web():
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8080)
 
 threading.Thread(target=run_web, daemon=True).start()  # Keep-alive thread
 
